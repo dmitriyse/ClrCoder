d589b4a359a2c39f5ed49ef2a7d4f5e92901b93d
dse <dse@sys-plex.com>
2015-07-07 10:42:23 +0400

    String extensions added.

---
+++ ClrCoder.System/StringExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ClrCoder.System
+{
+    /// <summary>
+    /// Utility methods for working with strings.
+    /// </summary>
+    public static class StringExtensions
+    {
+        //// TODO: Write exceptions description.
+
+        /// <summary>
+        /// Limits string length. 
+        /// </summary>
+        /// <param name="str"><c>String</c> to truncate.</param>
+        /// <param name="maxLength">Maximal allowed length.</param>
+        /// <returns>Truncated string.</returns>
+        public static string Truncate(this string str, int maxLength)
+        {
+            if (str == null)
+            {
+                throw new ArgumentNullException("str");
+            }
+
+            if (maxLength < 0)
+            {
+                throw new ArgumentOutOfRangeException("maxLength", "Maximal length should be positive.");
+            }
+            
+            if (str.Length > maxLength)
+            {
+                return str.Substring(0, maxLength);
+            }
+            
+            return str;
+        }
+    }
+}
