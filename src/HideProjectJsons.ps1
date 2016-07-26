bfadf6885dd41332a560ae35547f3d010fc5f33a
dse <dse@sys-plex.com>
2016-07-26 17:13:07 +0400

    NuGet issue was fixed.

--- src/HideProjectJsons.ps1
+++
@@ -1,2 +0,0 @@
-﻿Get-ChildItem -Recurse -Include project.json | Rename-Item -NewName { $_.Name.replace(".json",".json.bak") }
-Get-ChildItem -Recurse -Include project.lock.json | Rename-Item -NewName { $_.Name.replace("lock.json","lock.json.bak") }
--- src/RestoreProjectJsons.ps1
+++
@@ -1,2 +0,0 @@
-﻿Get-ChildItem -Recurse -Include project.json.bak | Rename-Item -NewName { $_.Name.replace(".json.bak",".json") }
-Get-ChildItem -Recurse -Include project.lock.json.bak | Rename-Item -NewName { $_.Name.replace(".lock.json.bak",".lock.json") }
