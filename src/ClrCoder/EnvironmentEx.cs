5ea0e5cc2318d5d597db238c121e1255411b9947
dse <dse@sys-plex.com>
2016-07-27 16:09:07 +0400

    Added Full framework target to ClrCoder.

---
+++ src/ClrCoder/EnvironmentEx.cs
@@ -0,0 +1,42 @@
+﻿// <copyright file="EnvironmentEx.cs" company="ClrCoder project">
+// Copyright (c) ClrCoder project. All rights reserved.
+// Licensed under the Apache 2.0 license. See LICENSE file in the project root for full license information.
+// </copyright>
+namespace ClrCoder
+{
+    using JetBrains.Annotations;
+
+    /// <summary>
+    /// Extended environment functions.
+    /// </summary>
+    [PublicAPI]
+    public static class EnvironmentEx
+    {
+        private static bool _isInitialized = false;
+
+        //// ReSharper disable once InconsistentNaming
+
+        /// <summary>
+        /// Checks if application executed under Linux OS.
+        /// </summary>
+        public static OSFamilyTypes OSFamily { get; private set; }
+
+#if PCL
+
+        /// <summary>
+        /// Initializes environment.
+        /// </summary>
+        /// <param name="osFamily">Specified OS family.</param>
+        public static void InitEnvironment(OSFamilyTypes? osFamily)
+        {
+            if (_isInitialized)
+            {
+                return;
+            }
+
+            OSFamily = osFamily ?? default(OSFamilyTypes);
+        }
+
+#endif
+    }
+}
---
+++ src/ClrCoder/OSFamilyType.cs
@@ -0,0 +1,38 @@
+﻿// <copyright file="OSFamilyType.cs" company="ClrCoder project">
+// Copyright (c) ClrCoder project. All rights reserved.
+// Licensed under the Apache 2.0 license. See LICENSE file in the project root for full license information.
+// </copyright>
+namespace ClrCoder
+{
+    using System;
+
+    using JetBrains.Annotations;
+
+    /// <summary>
+    /// Operation system family type.
+    /// </summary>
+    [PublicAPI]
+    [Flags]
+    public enum OSFamilyTypes
+    {
+        /// <summary>
+        /// Portable environment or unknown yet platform.
+        /// </summary>
+        Portable = 0, 
+
+        /// <summary>
+        /// Windows OS family.
+        /// </summary>
+        Windows = 0x1, 
+
+        /// <summary>
+        /// Linux OS family.
+        /// </summary>
+        Linux = 0x02 | Posix, 
+
+        /// <summary>
+        /// Posix OS - any Linux,Unix,MacOS, etc.
+        /// </summary>
+        Posix = 0x1000, 
+    }
+}
