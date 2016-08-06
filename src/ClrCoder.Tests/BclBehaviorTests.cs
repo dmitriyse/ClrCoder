36f7645dbf618881b3ee2700a2180c47dfba96c0
dse <dse@sys-plex.com>
2016-08-07 03:01:46 +0400

    Unix signals listener for AspNetCore shutdown added.

---
+++ src/ClrCoder.Tests/BclBehaviorTests.cs
@@ -0,0 +1,54 @@
+﻿// <copyright file="BclBehaviorTests.cs" company="ClrCoder project">
+// Copyright (c) ClrCoder project. All rights reserved.
+// Licensed under the Apache 2.0 license. See LICENSE file in the project root for full license information.
+// </copyright>
+namespace ClrCoder.Tests
+{
+    using System;
+    using System.Diagnostics;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using FluentAssertions;
+
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Identifies behavior of BCL parts.
+    /// </summary>
+    [TestFixture]
+    public class BclBehaviorTests
+    {
+        /// <summary>
+        /// Long running TPL task should use background thread. This behavior required to implement supporting background services that is terminated on application exit.
+        /// </summary>
+        [Test]
+        public void LongRunningTaskShouldUseBackgroundThreads()
+        {
+            var cts = new CancellationTokenSource();
+            var task = Task.Factory.StartNew(
+                () =>
+                    {
+                        Thread.CurrentThread.IsBackground.Should()
+                            .BeTrue("Assuming long running task spawn background thread.");
+
+                        // Performing wait that will be terminated.
+                        try
+                        {
+                            cts.Token.WaitHandle.WaitOne();
+                        }
+                        catch (ThreadAbortException)
+                        {
+                            // This code block actually executed, but NUnit skips this event.
+                            // Inspect this message in Debug output window.
+                            Trace.WriteLine("Gracefull termination");
+                        }
+
+                        // Right after the catch runtime will rise ThreadAbortException again.
+                        Trace.WriteLine("Unreachable code!");
+                    }, 
+                TaskCreationOptions.LongRunning);
+            task.Wait(TimeSpan.FromSeconds(1)).Should().BeFalse();
+        }
+    }
+}
