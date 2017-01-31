ede2b57b581103c3ebf81d7a1bb37fe8cd376603
var1ap <varushin-a-p@mail.ru>
2017-01-31 11:41:40 +0400

    Added helper functions for random set generation.

--- src/ClrCoder.Tests/BclProductivityExtensionsTest.cs
+++ src/ClrCoder.Tests/BclProductivityExtensionsTest.cs
@@ -5,6 +5,9 @@
 
 namespace ClrCoder.Tests
 {
+    using System;
+    using System.Collections.Generic;
+
     using FluentAssertions;
 
     using NUnit.Framework;
@@ -15,6 +18,38 @@ namespace ClrCoder.Tests
     [TestFixture]
     public class BclProductivityExtensionsTest
     {
+        /// <summary>
+        /// Test for the <see cref="BclProductivityExtensions.RandomUniqueSet"/> method.
+        /// </summary>
+        /// <param name="minValue">Test set size.</param>
+        /// <param name="maxValue">Test subset size.</param>
+        /// <param name="size"></param> 
+        [Test]
+        [TestCase(0, 0, 0)]
+        [TestCase(0, 1, 1)]
+        [TestCase(0, 17, 5)]
+        [TestCase(19999, 20000, 1)]
+        [TestCase(1, 20000, 19999)]
+        [TestCase(3, 17, 0)]
+        [TestCase(-100, -50, 30)]
+        [TestCase(50, 100, 30)]
+        [TestCase(7, 10, 2)]
+        public void RandomSubset_chekSeveralSubsetInSets_returnCorectRandomSubset(int minValue, int maxValue, int size)
+        {
+            var rnd = new Random();
+            List<int> randomSubset = rnd.RandomUniqueSet(minValue, maxValue, size);
+
+            foreach (int v in randomSubset)
+            {
+                v.Should()
+                    .BeGreaterOrEqualTo(minValue).And.BeLessThan(maxValue);
+            }
+
+            randomSubset.Should()
+                .HaveCount(size)
+                .And.OnlyHaveUniqueItems();
+        }
+
         /// <summary>
         /// Test for the <see cref="BclProductivityExtensions.ToDecimal"/>.
         /// </summary>
