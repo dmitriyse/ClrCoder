8dd81002c1577b7086a7f63b7a09eb40a223014b
var1ap <varushin-a-p@mail.ru>
2017-01-31 14:30:02 +0400

    Added helpers for intervals API.

---
+++ src/ClrCoder/Logic/IntervalExtensions.cs
@@ -0,0 +1,24 @@
+﻿// <copyright file="IntervalExtensions.cs" company="ClrCoder project">
+// Copyright (c) ClrCoder project. All rights reserved.
+// Licensed under the Apache 2.0 license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace ClrCoder.Logic
+{
+    /// <summary>
+    /// <see cref="Interval{T}"/> helper extensions.
+    /// </summary>
+    public static class IntervalExtensions
+    {
+        /// <summary>
+        /// Use int value as a start for <see cref="Interval{T}"/>.
+        /// </summary>
+        /// <param name="start">Int value to use as start.</param>
+        /// <param name="length">Length of the interval</param>
+        /// <returns>Interval from the specified value with the specified length.</returns>
+        public static Interval<int> WithLength(this int start, int length)
+        {
+            return new Interval<int>(start, start + length);
+        }
+    }
+}
