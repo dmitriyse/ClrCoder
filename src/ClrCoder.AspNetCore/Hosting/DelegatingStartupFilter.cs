0c7473837196d98fc8609011bc1d37289d4b79b8
dse <dse@sys-plex.com>
2017-03-10 00:12:54 +0400

    Added exception handling method into HostUtils.HostController<T>, DelegatingStartupFilter.

---
+++ src/ClrCoder.AspNetCore/Hosting/DelegatingStartupFilter.cs
@@ -0,0 +1,39 @@
+﻿// <copyright file="DelegatingStartupFilter.cs" company="ClrCoder project">
+// Copyright (c) ClrCoder project. All rights reserved.
+// Licensed under the Apache 2.0 license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace ClrCoder.AspNetCore.Hosting
+{
+    using System;
+
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+
+    /// <summary>
+    /// Startup filter that delegates it work.
+    /// </summary>
+    public class DelegatingStartupFilter : IStartupFilter
+    {
+        private readonly Action<IApplicationBuilder> _appInitializedDelegate;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DelegatingStartupFilter"/> class.
+        /// </summary>
+        /// <param name="appInitializedDelegate">App initializer delegate.</param>
+        public DelegatingStartupFilter(Action<IApplicationBuilder> appInitializedDelegate)
+        {
+            _appInitializedDelegate = appInitializedDelegate;
+        }
+
+        /// <inheritdoc/>
+        public Action<IApplicationBuilder> Configure(Action<IApplicationBuilder> next)
+        {
+            return app =>
+                {
+                    _appInitializedDelegate(app);
+                    next(app);
+                };
+        }
+    }
+}
--- src/ClrCoder.AspNetCore/Hosting/HostUtils.cs
+++ src/ClrCoder.AspNetCore/Hosting/HostUtils.cs
@@ -5,12 +5,16 @@
 
 namespace ClrCoder.AspNetCore.Hosting
 {
+#pragma warning disable 1998
 #if NET46 || NETSTANDARD1_6
     using System;
     using System.IO;
+    using System.Net;
 
     using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Diagnostics;
     using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.DependencyInjection;
 
     /// <summary>
     /// Asp.Net core hosting utils.
@@ -23,10 +27,12 @@ namespace ClrCoder.AspNetCore.Hosting
         /// <typeparam name="TController">Type of controller to host.</typeparam>
         /// <param name="urls">Hosting <c>urls</c>.</param>
         /// <param name="hostBuilderAction">Performs additional steps to build host.</param>
+        /// <param name="exceptionHandler">Hosting global exception handler.</param>
         /// <returns>Configured and running asp.net core host.</returns>
         public static IWebHost HostController<TController>(
             string urls,
-            Func<IWebHostBuilder, IWebHostBuilder> hostBuilderAction = null)
+            Func<IWebHostBuilder, IWebHostBuilder> hostBuilderAction = null,
+            Action<Exception> exceptionHandler = null)
         {
             if (urls == null)
             {
@@ -39,6 +45,38 @@ namespace ClrCoder.AspNetCore.Hosting
                 .UseOnlyController<TController>()
                 .Configure(
                     app => { app.UseMvc(); });
+
+            if (exceptionHandler != null)
+            {
+                builder.ConfigureServices(
+                    services =>
+                        {
+                            services.AddSingleton<IStartupFilter>(
+                                new DelegatingStartupFilter(
+                                    app =>
+                                        {
+                                            app.UseExceptionHandler(
+                                                new ExceptionHandlerOptions
+                                                    {
+                                                        ExceptionHandler =
+                                                            async context =>
+                                                                {
+                                                                    if (context.Response.StatusCode
+                                                                        == (int)HttpStatusCode.InternalServerError)
+                                                                    {
+                                                                        var errorFeature = context
+                                                                            .Features
+                                                                            .Get<IExceptionHandlerFeature>();
+                                                                        if (errorFeature != null)
+                                                                        {
+                                                                            exceptionHandler(errorFeature.Error);
+                                                                        }
+                                                                    }
+                                                                }
+                                                    });
+                                        }));
+                        });
+            }
 
             if (hostBuilderAction != null)
             {
