b907c5c3a4947b9c79f6f3a1e6adc4d3def1f117
dse <dse@sys-plex.com>
2017-03-21 18:06:47 +0400

    CanBeEmpty annotation attribute added.

---
+++ src/ClrCoder/Annotations/CanBeEmptyAttribute.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="CanBeEmptyAttribute.cs" company="ClrCoder project">
+// Copyright (c) ClrCoder project. All rights reserved.
+// Licensed under the Apache 2.0 license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace ClrCoder.Annotations
+{
+    using System;
+
+    using JetBrains.Annotations;
+
+    /// <summary>
+    /// Annotates that specified target can be empty.
+    /// </summary>
+    [PublicAPI]
+    [AttributeUsage(
+        AttributeTargets.Class
+        | AttributeTargets.Interface
+        | AttributeTargets.Field
+        | AttributeTargets.Property
+        | AttributeTargets.ReturnValue
+        | AttributeTargets.Parameter
+        | AttributeTargets.Method)]
+    public class CanBeEmptyAttribute : Attribute
+    {
+    }
+}
