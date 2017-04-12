2a36747d27a78e4943fb6c2f4707ff3c9e2796a1
dse <dse@sys-plex.com>
2017-04-12 05:48:52 +0400

    ActiveWorker utils added.

---
+++ src/ClrCoder/Threading/ActiveWorkerExtensions.cs
@@ -0,0 +1,41 @@
+﻿// <copyright file="ActiveWorkerExtensions.cs" company="ClrCoder project">
+// Copyright (c) ClrCoder project. All rights reserved.
+// Licensed under the Apache 2.0 license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace ClrCoder.Threading
+{
+    using System;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Extension methods related to the <see cref="ActiveWorker"/> class.
+    /// </summary>
+    public static class ActiveWorkerExtensions
+    {
+        /// <summary>
+        /// TODO: Document me.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="task"></param>
+        /// <param name="workItem"></param>
+        /// <returns></returns>
+        public static async Task<T> WithBlocker<T>(this IActiveWorkItem workItem, Func<Task<T>> action)
+        {
+            if (action == null)
+            {
+                throw new ArgumentNullException(nameof(action));
+            }
+
+            if (workItem == null)
+            {
+                throw new ArgumentNullException(nameof(workItem));
+            }
+
+            using (workItem.EnterWorkBlocker())
+            {
+                return await action();
+            }
+        }
+    }
+}
