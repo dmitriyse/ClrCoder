a12a24eb3fd7e7dffcace464168880b7c2025003
dse <dse@sys-plex.com>
2017-04-18 03:12:10 +0400

    Cross-platform HttpClient result detection added.

--- src/ClrCoder.Tests/Net/Http/FlurlBehaviorTests.cs
+++ src/ClrCoder.Tests/Net/Http/FlurlBehaviorTests.cs
@@ -5,13 +5,30 @@
 
 namespace ClrCoder.Tests.Net.Http
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Net.Http;
+    using System.Threading;
+    using System.Threading.Tasks;
+
     using ClrCoder.Json;
+    using ClrCoder.Logging;
+    using ClrCoder.Net.Http;
+    using ClrCoder.Threading;
+    using ClrCoder.AspNetCore.Hosting;
+
+    using FluentAssertions;
+
+    using Microsoft.AspNetCore.Mvc;
 
     using Flurl;
+    using Flurl.Http;
 
     using Newtonsoft.Json;
 
     using NUnit.Framework;
+
+    using Testing;
 
     /// <summary>
     /// Tests flurl implementation.
@@ -19,6 +36,60 @@ namespace ClrCoder.Tests.Net.Http
     [TestFixture]
     public class FlurlBehaviorTests
     {
+        /// <summary>
+        /// Test for connecting to unknown DNS name.
+        /// </summary>
+        /// <param name="host"> The <c>host</c>. </param>
+        /// <param name="dnsOrIp"> The dns or IP. </param>
+        /// <param name="pathSegment"> The path Segment. </param>
+        /// <returns>
+        /// Async execution TPL task.
+        /// </returns>
+        [Test]
+        [TestCase("http://localhost:5001", "http://localhost:5001", "Flurl")]
+        [TestCase("http://localhost:5001", "http://localhost:5001", "nFlurl")]
+        [TestCase("http://localhost:5001", "http://nobody-knows-me-99923.com", "Flurl")]
+        [TestCase("http://localhost:5001", "http://10.110.110.110/", "nFlurl")]
+        [TestCase("http://localhost:5001", "http://46.20.65.243:22222/", "")]
+        public async Task FlurlHttpExtensionsTest(string host, string dnsOrIp, string pathSegment)
+        {
+            try
+            {
+                using (HostUtils.HostController<TestFlurlController>(host))
+                {
+                    string response = await new Url(dnsOrIp).AppendPathSegment(pathSegment).GetAsync().ReceiveString();
+                    if (pathSegment == "Flurl" && host == "http://localhost:5001")
+                    {
+                        response.Should().BeEquivalentTo("Hello");
+                    }
+                }
+            }
+            catch (FlurlHttpTimeoutException)
+            {
+                TestContext.WriteLine("FlurlHttpTimeoutException");
+            }
+            catch (FlurlHttpException ex)
+            {
+                if (dnsOrIp == "http://nobody-knows-me-99923.com")
+                {
+                    ex.IsDnsResolveError().Should().Be(true);
+                }
+                else
+                {
+                    ex.IsDnsResolveError().Should().Be(false);
+                }
+
+                if (dnsOrIp == "http://10.110.110.110/" || dnsOrIp == "http://46.20.65.243:22222/")
+                {
+                    ex.IsIpNotAvailable().Should().Be(true);
+                }
+                else
+                {
+                    ex.IsIpNotAvailable().Should().Be(false);
+                }
+            }
+        }
+
         /// <summary>
         /// Tests json serialization behavior.
         /// </summary>
@@ -30,6 +101,22 @@ namespace ClrCoder.Tests.Net.Http
                 new Url("http://test.com"),
                 JsonDefaults.JsonConfigSerializerSource.Settings);
             TestContext.WriteLine(result);
+        }
+
+        [Route("/Flurl")]
+        public class TestFlurlController
+        {
+            [HttpGet]
+            public string Get()
+            {
+                return "Hello";
+            }
+
+            [HttpPost]
+            public string Post()
+            {
+                return "Bye";
+            }
         }
     }
 }
--- src/ClrCoder/Net/Http/FlurlHttpExtensions.cs
+++ src/ClrCoder/Net/Http/FlurlHttpExtensions.cs
@@ -7,6 +7,7 @@ namespace ClrCoder.Net.Http
 {
 #if NET46 || NETSTANDARD1_6
     using System;
+    using System.Net;
     using System.Net.Http;
     using System.Net.Http.Headers;
     using System.Threading;
@@ -26,7 +27,185 @@ namespace ClrCoder.Net.Http
     public static class FlurlHttpExtensions
     {
         /// <summary>
-        /// Posts binary data to the url.
+        /// TODO: Document me.
+        /// </summary>
+        /// <param name="flurlException"></param>
+        /// <returns></returns>
+        public static bool IsCertificateError(this FlurlHttpException flurlException)
+        {
+            // core
+            if (flurlException.Call.Response == null
+                && flurlException.InnerException?.GetType() == typeof(HttpRequestException)
+                && flurlException.InnerException?.InnerException?.Message ==
+                "A security error occurred")
+            {
+                return true;
+            }
+
+            // net 4.6
+            if (flurlException.Call.Response == null
+                && flurlException.InnerException?.GetType() == typeof(HttpRequestException)
+                && (flurlException.InnerException?.InnerException?.Message?.Contains("A security error occurred")
+                    ?? false)
+                && (flurlException.InnerException?.InnerException?.InnerException?.Message?.Contains("handshake failed")
+                    ?? false))
+            {
+                return true;
+            }
+
+            // Linux
+            if (flurlException.Call.Response == null
+                && flurlException.InnerException?.GetType() == typeof(HttpRequestException)
+                && (flurlException.InnerException?.InnerException?.Message?.Contains("SSL") ?? false))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// TODO: Document me.
+        /// </summary>
+        /// <param name="flurlException"></param>
+        /// <returns></returns>
+        public static bool IsDnsResolveError(this FlurlHttpException flurlException)
+        {
+            if (flurlException == null)
+            {
+                throw new ArgumentNullException(nameof(flurlException));
+            }
+
+            // net 4.6
+            if (flurlException.Call.Response == null
+                && flurlException.InnerException?.GetType() == typeof(HttpRequestException)
+                && flurlException.InnerException.InnerException.Message != null
+                && flurlException.InnerException.InnerException.Message.StartsWith(
+                    "remote name could not be resolved"))
+            {
+                return true;
+            }
+
+            // core
+            if (flurlException.Call.Response == null
+                && flurlException.InnerException?.GetType() == typeof(HttpRequestException)
+                && (flurlException.InnerException?.InnerException?.Message?.Contains(
+                        "server name or address could not be resolved") ?? false))
+            {
+                return true;
+            }
+
+            // Linux
+            return flurlException.Call.Response == null
+                   && flurlException.InnerException?.GetType() == typeof(HttpRequestException)
+                   && flurlException.InnerException?.InnerException?.Message != null
+                   && flurlException.InnerException.InnerException.Message.Contains("resolve host name");
+        }
+
+        /// <summary>
+        /// TODO: Document me.
+        /// </summary>
+        /// <param name="flurlException"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static bool IsIpNotAvailable(this FlurlHttpException flurlException)
+        {
+            if (flurlException == null)
+            {
+                throw new ArgumentNullException(nameof(flurlException));
+            }
+
+            // net 4.6 does not exist IP
+            if (flurlException.Call.Response == null
+                && flurlException.InnerException?.GetType() == typeof(HttpRequestException)
+                && flurlException.InnerException.InnerException.InnerException != null
+                && flurlException.InnerException?.InnerException?.Message == "Unable to connect to the remote server"
+                && flurlException.InnerException.InnerException.InnerException.Message.StartsWith(
+                    "A connection attempt failed because the connected party did not properly respond after a period of time, or established connection failed because connected host has failed to respond")
+            )
+            {
+                return true;
+            }
+
+            // net 4.6 locked IP
+            if (flurlException.Call.Response == null
+                && flurlException.InnerException?.GetType() == typeof(HttpRequestException)
+                && flurlException.InnerException.InnerException.InnerException != null
+                && flurlException.InnerException?.InnerException?.Message == "Unable to connect to the remote server"
+                && flurlException.InnerException.InnerException.InnerException.Message.StartsWith(
+                    "No connection could be made because the target machine actively refused it"))
+            {
+                return true;
+            }
+
+            // core does not exist IP
+            if (flurlException.Call.Response == null
+                && flurlException.InnerException?.GetType() == typeof(HttpRequestException)
+                && flurlException.InnerException?.InnerException?.Message ==
+                "The operation timed out")
+            {
+                return true;
+            }
+
+            // core locked IP
+            if (flurlException.Call.Response == null
+                && flurlException.InnerException?.GetType() == typeof(HttpRequestException)
+                && flurlException.InnerException?.InnerException?.Message ==
+                "A connection with the server could not be established")
+            {
+                return true;
+            }
+
+            // Linux does not exist IP
+            // FlurlHttpException
+            // Request to http://10.110.110.110/ failed. A task was canceled.
+            // TaskCanceledException
+            // A task was canceled.
+
+            // Linux locked IP
+            if (flurlException.Call.Response == null
+                && flurlException.InnerException?.GetType() == typeof(HttpRequestException)
+                && flurlException.InnerException?.InnerException?.Message ==
+                "Couldn't connect to server")
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// TODO: Document me.
+        /// </summary>
+        /// <param name="flurlException"></param>
+        /// <param name="exceptionToken"></param>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        public static bool IsTimeOutError(
+            this FlurlHttpException flurlException,
+            CancellationToken exceptionToken,
+            CancellationToken token)
+        {
+            if (flurlException.Call.HttpStatus == HttpStatusCode.RequestTimeout)
+            {
+                return true;
+            }
+
+            // 4.6 and core and Linux
+            // if (flurlException.InnerException?.Message == "A task was canceled.")
+            // {
+            // return true;
+            // }
+            if (!(exceptionToken == token))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Posts binary data to the flurl client.
         /// </summary>
         /// <param name="client">Flurl fluent syntax.</param>
         /// <param name="content">Binary content to send.</param>
