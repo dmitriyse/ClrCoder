776a7e226c38fe9942146e752eb965e1c994b9f4
malinyak <none@none>
2017-04-28 10:24:44 +0400

    Log reader contract added

---
+++ src/ClrCoder/Logging/Std/ILogReader.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="ILogReader.cs" company="ClrCoder project">
+// Copyright (c) ClrCoder project. All rights reserved.
+// Licensed under the Apache 2.0 license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace ClrCoder.Logging.Std
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    using Logic;
+
+    using NodaTime;
+
+    public interface ILogReader
+    {
+        ValueTask<IReadOnlyList<LogEntry>> GetLogs(Interval<Instant> interval);
+    }
+}
