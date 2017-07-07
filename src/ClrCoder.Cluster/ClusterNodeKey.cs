ee161cf64c734d648f8dadfd8619fa429c96eb29
dse <dse@sys-plex.com>
2017-07-07 11:14:35 +0400

    Small changes.

---
+++ src/ClrCoder.Cluster/ClusterNodeKey.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="ClusterNodeKey.cs" company="ClrCoder project">
+// Copyright (c) ClrCoder project. All rights reserved.
+// Licensed under the Apache 2.0 license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace ClrCoder.Cluster
+{
+    /// <summary>
+    /// </summary>
+    public class ClusterNodeKey
+    {
+        /// <summary>
+        /// Human memorable short node identifier.
+        /// </summary>
+        /// <remarks>
+        /// Something like dc1-role2-03. Where 03 is a machine number. Example azure-europe-worker-05.
+        /// </remarks>
+        public string Code { get; set; }
+    }
+}
