1631b4372a5f8a6ebfa4bf2183ce9aee65c21ab5
dse <dse@sys-plex.com>
2018-01-18 22:38:29 +0400

    json serialization camel case fix for dictionaries.

---
+++ src/ClrCoder/Json/CamelCaseExceptDictionaryKeysResolver.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="CamelCaseExceptDictionaryKeysResolver.cs" company="ClrCoder project">
+// Copyright (c) ClrCoder project. All rights reserved.
+// Licensed under the Apache 2.0 license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace ClrCoder.Json
+{
+    using System;
+
+    using Newtonsoft.Json.Serialization;
+
+    /// <summary>
+    /// Standard camel case contract resolver fix. Dictionary keys case not modified.
+    /// </summary>
+    public class CamelCaseExceptDictionaryKeysResolver : CamelCasePropertyNamesContractResolver
+    {
+        /// <inheritdoc/>
+        protected override JsonDictionaryContract CreateDictionaryContract(Type objectType)
+        {
+            JsonDictionaryContract contract = base.CreateDictionaryContract(objectType);
+
+            contract.DictionaryKeyResolver = propertyName => propertyName;
+
+            return contract;
+        }
+    }
+}
