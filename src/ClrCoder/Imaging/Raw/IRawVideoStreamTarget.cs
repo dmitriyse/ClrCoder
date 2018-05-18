b59f920a7982b413b11a0c9c82a477f0ca88c2cf
dse <dse@sys-plex.com>
2018-05-18 17:53:46 +0400

    Raw video stream target abstraction has been added.

---
+++ src/ClrCoder/Imaging/Raw/IRawVideoStreamTarget.cs
@@ -0,0 +1,29 @@
+﻿// <copyright file="IRawVideoStreamTarget.cs" company="ClrCoder project">
+// Copyright (c) ClrCoder project. All rights reserved.
+// Licensed under the Apache 2.0 license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+#if !NETSTANDARD1_0 && !NETSTANDARD1_1 && !NETSTANDARD1_3
+namespace ClrCoder.Imaging.Raw
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// The raw video stream target.
+    /// </summary>
+    public interface IRawVideoStreamTarget : IAsyncDisposable
+    {
+        /// <summary>
+        /// The video format of the source.
+        /// </summary>
+        RawVideoStreamFormat VideoFormat { get; }
+
+        /// <summary>
+        /// Opens a raw video writer.
+        /// </summary>
+        /// <returns>The video writer interface.</returns>
+        ValueTask<IRawVideoStreamWriter> OpenWriter();
+    }
+}
+#endif
---
+++ src/ClrCoder/Imaging/Raw/IRawVideoStreamWriter.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="IRawVideoStreamWriter.cs" company="ClrCoder project">
+// Copyright (c) ClrCoder project. All rights reserved.
+// Licensed under the Apache 2.0 license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+#if !NETSTANDARD1_0 && !NETSTANDARD1_1 && !NETSTANDARD1_3
+namespace ClrCoder.Imaging.Raw
+{
+    using System.Threading;
+
+    using Threading.Channels;
+
+    /// <summary>
+    /// The raw video writer contract.
+    /// </summary>
+    public interface IRawVideoStreamWriter : IChannelWriter<IRawVideoFrame>, IAsyncDisposable
+    {
+    }
+}
+#endif
